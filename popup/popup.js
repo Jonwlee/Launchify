e298a92d9c40177a8f1c1b7d348e952ed3f17d38
Jonathan Lee <jonwlee457@gmail.com>
2015-05-23 23:17:07 -0400

    It works, ejs was weird had to  enable unsafe
    spitter works

--- popup/popup.js
+++ popup/popup.js
@@ -8,21 +8,28 @@ var launchStorageKey = "launches";
 // 	"sites": ["twitter.com","google.com"]
 // }]
 
-
-
 $(document).ready(function () {
 	$('#tabs').tab();
 
 	//Retrieve data
-	chrome.storage.sync.get(launchStorageKey, function(items) {
-		if (typeof items.sets === "undefined") {
-			items.sets = [];
-			chrome.storage.set({launchStorageKey: items.sets});
+	chrome.storage.sync.get('sets', function(result) {
+		var sets = result.sets;
+
+		if (typeof result === "undefined" || typeof sets === "undefined") {
+			sets = [];
+			result = {sets: sets};
+			chrome.storage.sync.set({'sets':result}, function() {
+				console.log("added");
+			});
 		}
-		var tabsHTML = new EJS({url: 'cleaning.ejs'}).render(items.sets); 
-		console.log(sets);
+		//Generate tabs html
+		var tabsHTML = new EJS({url: 'tabs.ejs'}).render(result);
+		
+		//Insert into page
+		$("#content").html(tabsHTML);
 	});
 
+	//
 });
 
 function launchify () { 
